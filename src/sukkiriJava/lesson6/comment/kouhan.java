92560adae282df8a6a2c2fcc3148ac1c2737dcfb
siryu-saito <saito.s.ryu@gmail.com>
2021-09-30 16:00:41 +0900

    6章 練習問題

---
+++ src/sukkiriJava/lesson6/comment/kouhan.java
@@ -0,0 +1,14 @@
+package sukkiriJava.lesson6.comment;
+
+public class kouhan {
+	public static void callDeae() {
+		System.out.println("えぇい、こしゃくな。くせものだ！であえい！");
+	}
+
+	public static void showMondokoro() throws Exception {
+		System.out.println("飛車さん、角さん。もういいでしょう。");
+		System.out.println("この紋所が目に入らぬか！");
+		Thread.sleep(10000);
+		zenhan.doTogame();
+	}
+}
---
+++ src/sukkiriJava/lesson6/main.java
@@ -0,0 +1,12 @@
+package sukkiriJava.lesson6;
+
+import sukkiriJava.lesson6.comment.zenhan;
+
+public class main {
+	public static void main(String[] args) throws Exception {
+		zenhan.doWarusa();
+		zenhan.doTogame();
+		sukkiriJava.lesson6.comment.kouhan.callDeae();
+		sukkiriJava.lesson6.comment.kouhan.showMondokoro();
+	}
+}
